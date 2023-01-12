06ccab449c640dd95766eee1e654ff2a1369320e
Douglas Vieira <douglasdotv@hotmail.com>
2023-01-12 13:56:37 -0300

    Implement stage 3 ("estimate the number of servings")

--- src/main/kotlin/coffeemachine/Main.kt
+++ src/main/kotlin/coffeemachine/Main.kt
@@ -1,19 +1,51 @@
 package coffeemachine
 
 fun main() {
-    println("Write how many cups of coffee you will need: ")
-    val cupsOfCoffee = readln().toInt()
-
-    calculateIngredients(cupsOfCoffee)
+    val ingredients = getIngredients()
+    val neededCupsOfCoffee = getNeededCoffee()
+    val maxCupsOfCoffee = calculateMaxCupsOfCoffee(ingredients)
+    runCoffeeMachine(neededCupsOfCoffee, maxCupsOfCoffee)
 }
 
-private fun calculateIngredients(cupsOfCoffee: Int) {
-    println(
-        """
-    For 125 cups of coffee you will need: 
-    ${cupsOfCoffee * 200} ml of water
-    ${cupsOfCoffee * 50} ml of milk
-    ${cupsOfCoffee * 15} g of coffee beans
-    """.trimIndent()
+private fun getIngredients(): Array<Int> {
+    val ingredientNames = arrayOf("water", "milk", "coffee")
+    val ingredients = Array(3) { 0 }
+
+    for (i in ingredientNames.indices) {
+        println("Write how many ml of ${ingredientNames[i]} the coffee machine has: ")
+        ingredients[i] = readln().toInt()
+    }
+
+    return ingredients
+}
+
+private fun getNeededCoffee(): Int {
+    println("Write how many cups of coffee you will need: ")
+    return readln().toInt()
+}
+
+fun calculateMaxCupsOfCoffee(ingredients: Array<Int>): Int {
+    val requiredIngredients = arrayOf(200, 50, 15)
+
+    if (ingredients[0] < requiredIngredients[0]
+        || ingredients[1] < requiredIngredients[1]
+        || ingredients[2] < requiredIngredients[2]
+    ) {
+        return 0
+    }
+
+    return minOf(
+        ingredients[0] / requiredIngredients[0],
+        ingredients[1] / requiredIngredients[1],
+        ingredients[2] / requiredIngredients[2]
     )
+}
+
+private fun runCoffeeMachine(needed: Int, max: Int) {
+    val diff = max - needed
+    when {
+        diff < 0 -> println("No, I can make only $max cups of coffee")
+        diff > 0 -> println("Yes, I can make that amount of coffee (and even $diff more than that)")
+        else -> println("Yes, I can make that amount of coffee")
+    }
 }
